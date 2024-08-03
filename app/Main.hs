07ed3f8da385dc621428ed51e16fd5962d91d1eb
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2024-08-03 14:05:23 +0200

    add dummy STM model server

--- app/Main.hs
+++ app/Main.hs
@@ -9,39 +9,61 @@ import qualified Brick.AttrMap
 import Brick.Widgets.Core (str)
 import Brick.Types ( Widget, BrickEvent(..) )
 import Brick.Types (EventM)
-import Control.Monad.State (modify, liftIO)
+import Control.Monad.State (modify, liftIO, MonadState (put, get))
 
 import System.Log.Logger (logL, Priority(ERROR, WARNING, INFO, DEBUG))
 import Log
+import Control.Concurrent.STM (TVar)
 
--- main :: IO ()
--- main = putStrLn "Hello, Haskell!" >> ffun 1 2
+import Model
+
+data State = State {
+  modelServer :: ModelServer,
+  cachedVal :: Int
+}
 
 main :: IO()
 main = do
   setupLogger
   glogL INFO "App starting"
-  void $ defaultMain app 1
+  modelServer <- startModelServer
+  model <- getModel modelServer
+  void $ defaultMain app (State modelServer (val model))
   glogL INFO "App did quit normally"
 
 myAttrMap :: Brick.AttrMap.AttrMap
 myAttrMap = themeToAttrMap $ newTheme (green `on` black) []
 
-ui :: Int -> Widget n
-ui i = str (show i)
+ui :: State -> Widget n
+ui s = str (show (cachedVal s))
 
-myHandleEvent :: BrickEvent () e -> EventM () Int ()
+myModifyModelState :: State -> (Model -> Model) -> IO State
+myModifyModelState s f = do
+  let ms = modelServer s
+  modifyModelOnServer ms f
+  model <- getModel ms
+  return s { cachedVal = val model }
+ 
+myHandleEvent :: BrickEvent () e -> EventM () State ()
 myHandleEvent ev = case ev of
   (VtyEvent (EvKey (KChar 'q') [])) -> do
     liftIO (glogL INFO "quitting...")
     halt
+
   (VtyEvent (EvKey (KChar '+') [])) -> do
     liftIO (glogL INFO "adding 1")
-    modify (+1)
-  (VtyEvent (EvKey (KChar '-') [])) -> modify (\x -> x-1)
+    -- TODO going forward, the server should send US (and everyone else) a message on update.
+    s <- get
+    s' <- liftIO (myModifyModelState s (\(Model i) -> Model (i+1)))
+    put s'
+
+  (VtyEvent (EvKey (KChar '-') [])) -> do
+    s <- get
+    s' <- liftIO (myModifyModelState s (\(Model i) -> Model (i-1)))
+    put s'
   _ -> return ()
 
-app :: App Int e ()
+app :: App State e ()
 app = App
   { appDraw = \s -> [ui s]
   , appHandleEvent = myHandleEvent
---
+++ app/Model.hs
@@ -0,0 +1,29 @@
+module Model(Model(..), ModelServer, getModel, startModelServer, modifyModelOnServer) where
+import Control.Concurrent.STM
+
+-- TODO dummy
+data Model = Model {
+  val :: Int
+}
+
+emptyModel :: Model
+emptyModel = Model 0
+
+-- SOMEDAY put this into a separate module
+
+-- There's not actually a server here but we *may* want to make it one later.
+newtype ModelServer = ModelServer (TVar Model)
+
+getModel :: ModelServer -> IO Model
+getModel (ModelServer mv) = readTVarIO mv
+
+-- TODO this prob shouldn't exist
+modifyModelOnServer :: ModelServer -> (Model -> Model) -> IO ()
+modifyModelOnServer (ModelServer mv) f = atomically $ modifyTVar' mv f
+
+-- TODO dummy
+startModelServer :: IO ModelServer
+startModelServer = do
+  mv <- newTVarIO emptyModel
+  return (ModelServer mv)
+
