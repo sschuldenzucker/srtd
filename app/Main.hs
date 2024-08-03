2ce8e8b61c9cd114b1a735a79e2779c402e78399
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2024-08-03 20:02:22 +0200

    more serious model and rendering (not doing anything rn but works)

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,34 @@
 module Main (main) where
 
-import Brick (on)
+import Brick (on, vBox, (<=>))
 import Brick.AttrMap qualified
 import Brick.Main
 import Brick.Themes
 import Brick.Types (BrickEvent (..), EventM, Widget)
+import Brick.Widgets.Center qualified as C
 import Brick.Widgets.Core (str)
+import Brick.Widgets.List qualified as L
 import Control.Concurrent.STM (TVar)
 import Control.Monad (void)
 import Control.Monad.State (MonadState (get, put), liftIO, modify)
+import Data.Tree (flatten)
+import Data.Vector qualified as Vec
 import Graphics.Vty (Event (..), Key (..), black, blue, green, magenta, white, yellow)
 import Log
 import Model
 import System.Log.Logger (Priority (DEBUG, ERROR, INFO, WARNING), logL)
+import Todo
 
-data State = State
+data AppState = AppState
   { modelServer :: ModelServer,
-    cachedVal :: Int
+    asRoot :: EID,
+    asFilter :: Filter,
+    asSubtree :: Subtree,
+    -- SOMEDAY we may wanna pre-render a bit more here. E.g. put the fully rendered stuff and the EID in there. Or something.
+    asList :: L.List AppResourceName (Int, EID, Attr)
   }
+
+data AppResourceName = MainList deriving (Eq, Ord, Show)
 
 main :: IO ()
 main = do
@@ -25,40 +36,53 @@ main = do
   glogL INFO "App starting"
   modelServer <- startModelServer
   model <- getModel modelServer
-  void $ defaultMain app (State modelServer (val model))
+  let subtree = runFilter f_identity Inbox model
+  void $
+    defaultMain
+      app
+      ( AppState
+          { modelServer = modelServer,
+            asRoot = Inbox,
+            asFilter = f_identity,
+            asSubtree = subtree,
+            asList = forestToBrickList (stForest subtree)
+          }
+      )
   glogL INFO "App did quit normally"
 
 myAttrMap :: Brick.AttrMap.AttrMap
 myAttrMap = themeToAttrMap $ newTheme (green `on` black) []
 
-ui :: State -> Widget n
-ui s = str (show (cachedVal s))
+ui :: AppState -> Widget AppResourceName
+-- ui _s = str "ok I guess TODO"
+-- TODO render root. Prob just another, unselected element.
+ui AppState {asList, asSubtree = Subtree {root, rootAttr}} = box
+  where
+    box = renderRow False rootRow <=> L.renderList renderRow True asList
+    rootRow = (-1, root, rootAttr)
+    renderRow :: Bool -> (Int, EID, Attr) -> Widget AppResourceName
+    -- todo use selected and set attrs
+    renderRow sel (lvl, _, Attr {name}) = str (concat (replicate (lvl + 1) "  ") ++ name)
 
-myModifyModelState :: State -> (Model -> Model) -> IO State
-myModifyModelState s f = do
-  let ms = modelServer s
-  modifyModelOnServer ms f
-  model <- getModel ms
-  return s {cachedVal = val model}
+forestToBrickList :: MForest -> L.List AppResourceName (Int, EID, Attr)
+forestToBrickList forest = L.list MainList (Vec.fromList contents) 1
+  where
+    contents = map (\(lvl, (i, attr)) -> (lvl, i, attr)) $ forestFlattenWithLevels forest
 
-myHandleEvent :: BrickEvent () e -> EventM () State ()
+myModifyModelState :: AppState -> (Model -> Model) -> IO AppState
+myModifyModelState s@(AppState {modelServer, asRoot, asFilter}) f = do
+  modifyModelOnServer modelServer f
+  model <- getModel modelServer
+  return s {asSubtree = runFilter asFilter asRoot model}
+
+myHandleEvent :: BrickEvent AppResourceName e -> EventM AppResourceName AppState ()
 myHandleEvent ev = case ev of
   (VtyEvent (EvKey (KChar 'q') [])) -> do
     liftIO (glogL INFO "quitting...")
     halt
-  (VtyEvent (EvKey (KChar '+') [])) -> do
-    liftIO (glogL INFO "adding 1")
-    -- TODO going forward, the server should send US (and everyone else) a message on update.
-    s <- get
-    s' <- liftIO (myModifyModelState s (\(Model i) -> Model (i + 1)))
-    put s'
-  (VtyEvent (EvKey (KChar '-') [])) -> do
-    s <- get
-    s' <- liftIO (myModifyModelState s (\(Model i) -> Model (i - 1)))
-    put s'
   _ -> return ()
 
-app :: App State e ()
+app :: App AppState e AppResourceName
 app =
   App
     { appDraw = \s -> [ui s],
--- app/Model.hs
+++ app/Model.hs
@@ -1,14 +1,105 @@
-module Model (Model (..), ModelServer, getModel, startModelServer, modifyModelOnServer) where
+module Model where
 
 import Control.Concurrent.STM
+import Data.Either (fromRight)
+import Data.List (find)
+import Data.Tree
+import Data.UUID (UUID)
+
+-- import Data.UUID.V4 (nextRandom)
+
+data EID = Inbox | Vault | EIDNormal (UUID) deriving (Eq)
+
+data Attr = Attr
+  { name :: String
+  }
+
+leaf :: a -> Tree a
+leaf x = Node x []
+
+type MForest = Forest (EID, Attr)
 
 -- TODO dummy
 data Model = Model
-  { val :: Int
+  { forest :: MForest
   }
 
 emptyModel :: Model
-emptyModel = Model 0
+emptyModel =
+  Model
+    [ leaf (Inbox, Attr "INBOX"),
+      leaf (Vault, Attr "VAULT")
+    ]
+
+-- SOMEDAY put these into a separate module
+
+data InsertLoc id
+  = Before id
+  | After id
+  | FirstChild id
+  | LastChild id
+
+-- TODO these should have Either return types b/c it's possible that the loc node was deleted just before we were trying to insert.
+-- Currently, insert does nothing in these cases.
+
+-- | *You* need to make sure that the provided uuid is actually new, probably using UUID.nextRandom.
+insertNewNormalWithNewId :: UUID -> Attr -> InsertLoc EID -> Model -> Model
+insertNewNormalWithNewId uuid attr loc (Model forest) = Model (forestInsertAtId loc (EIDNormal uuid, attr) forest)
+
+-- There's probably some clever monadic way of doing this but I'm not smart enough for that.
+
+forestInsertAtId :: (Eq id) => InsertLoc id -> (id, a) -> Forest (id, a) -> Forest (id, a)
+forestInsertAtId (Before tgt) idattr (n@(Node (i, _) _) : rst) | i == tgt = Node idattr [] : n : rst
+forestInsertAtId (After tgt) idattr (n@(Node (i, _) _) : rst) | i == tgt = n : Node idattr [] : rst
+forestInsertAtId loc idattr forest = map (treeInsertAtId loc idattr) forest
+
+treeInsertAtId :: (Eq id) => InsertLoc id -> (id, a) -> Tree (id, a) -> Tree (id, a)
+treeInsertAtId (FirstChild tgt) idattr (Node (i, attr) children) | i == tgt = Node (i, attr) (Node idattr [] : children)
+treeInsertAtId (LastChild tgt) idattr (Node (i, attr) children) | i == tgt = Node (i, attr) (children ++ [Node idattr []])
+treeInsertAtId loc idattr (Node (i, attr) children) = Node (i, attr) (forestInsertAtId loc idattr children)
+
+data Subtree = Subtree
+  { breadcrumbs :: [EID],
+    root :: EID,
+    rootAttr :: Attr,
+    stForest :: MForest
+  }
+
+data IdNotFoundError = IdNotFoundError
+
+modelGetSubtreeBelow :: EID -> Model -> Either IdNotFoundError Subtree
+modelGetSubtreeBelow i (Model forest) = forestGetSubtreeBelow i forest
+
+-- SOMEDAY some of the following could probably be further abstracted away by supplying a condition on either the tree labels or subtrees themselves.
+
+-- | All subtrees (with repetitions of children) with breadcrumbs (parents), in preorder.
+forestTrees :: Forest a -> [([a], Tree a)]
+forestTrees = concatMap (goTree [])
+  where
+    goTree crumbs n@(Node x children) = (crumbs, n) : concatMap (goTree (x : crumbs)) children
+
+-- | Preorder nodes with their respecive levels
+forestFlattenWithLevels :: Forest a -> [(Int, a)]
+forestFlattenWithLevels = map extr . forestTrees
+  where
+    extr (crumbs, (Node x _)) = (length crumbs, x)
+
+forestFindTree :: (Eq id) => id -> Forest (id, a) -> Maybe ([id], Tree (id, a))
+forestFindTree tgt forest = find (\(_, Node (i, _) _) -> i == tgt) $ treesWithIdBreadcrumbs
+  where
+    -- Mogrify b/c forestTrees also returns the attrs, which we don't care about here.
+    treesWithIdBreadcrumbs = map (\(bs, t) -> (map fst bs, t)) $ forestTrees forest
+
+forestGetSubtreeBelow :: EID -> Forest (EID, Attr) -> Either IdNotFoundError Subtree
+forestGetSubtreeBelow tgt forest = case forestFindTree tgt forest of
+  Just (crumbs, (Node (i, attr) cs)) -> Right (Subtree crumbs i attr cs)
+  Nothing -> Left IdNotFoundError
+
+-- SOMEDAY unclear if this is the right structure.
+newtype Filter = Filter {runFilter :: EID -> Model -> Subtree}
+
+-- SOMEDAY decide on error handling
+f_identity = Filter (\i m -> fromRight (error "root EID not found") $ modelGetSubtreeBelow i m)
 
 -- SOMEDAY put this into a separate module
 
