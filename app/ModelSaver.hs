57c99482b9e1ecf308465e23d2f086484af6407e
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2024-08-05 10:19:37 +0200

    ModelSaver: re-throw exception in parent
    not sure this is how it's supposed to be but w/e. Right now we just
    wanna make it crash.

--- app/ModelSaver.hs
+++ app/ModelSaver.hs
@@ -5,6 +5,7 @@ module ModelSaver where
 
 import Config (model_filename)
 import Control.Concurrent
+import Control.Exception (SomeException, catch)
 import Control.Monad (forever)
 import Data.Aeson.Encode.Pretty
 import Data.ByteString.Lazy qualified as B
@@ -14,14 +15,19 @@ import ModelServer qualified as MS
 -- SOMEDAY some handling of crashes (at least re-raise exceptions, or honestly restart like Erlang)
 startModelSaver :: MS.ModelServer -> IO ThreadId
 startModelSaver mserver = do
+  parentThread <- myThreadId
   chan <- newChan
   MS.subscribe mserver (writeChan chan)
-  forkIO $ forever $ do
-    -- SOMEDAY do I wanna throttle the rate at which updates are processed?
-    -- (in case there are many for some reason)
-    _ <- readChan chan
-    model <- MS.getModel mserver
-    writeModelToFile model
+  let server = forever $ do
+        -- SOMEDAY do I wanna throttle the rate at which updates are processed?
+        -- (in case there are many for some reason)
+        _ <- readChan chan
+        model <- MS.getModel mserver
+        writeModelToFile model
+  let handler :: SomeException -> IO ()
+      handler = throwTo parentThread
+  -- I'm not 100% sure this is the right way. Feels unideomatic.
+  forkIO (server `catch` handler)
 
 writeModelToFile :: Model -> IO ()
 writeModelToFile model = B.writeFile model_filename (encodePretty' prettyConfig model)
