d5816c2a2ba4247564192a1d0f0dc4410f233ae2
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2024-08-11 00:31:44 +0200

    add alignment helpers for bottom-right alignment

---
+++ app/Alignment.hs
@@ -0,0 +1,65 @@
+-- | Alignment helpers for brick, to get e.g. bottom-right alignment.
+--
+-- Kinda shocking it doesn't have that tbh.
+module Alignment where
+
+import Brick.Types
+import Brick.Widgets.Core
+import Data.Maybe (fromMaybe)
+import Graphics.Vty
+  ( charFill,
+    horizCat,
+    imageHeight,
+    imageWidth,
+    translateX,
+    translateY,
+    vertCat,
+  )
+import Lens.Micro.Platform (to, (&), (.~), (^.))
+
+-- | Counterpart to hCenterLayer that aligns right.
+--
+-- Mostly copied from `hCenterLayer`
+hAlignRightLayer :: Widget n -> Widget n
+hAlignRightLayer p =
+  Widget Greedy (vSize p) $ do
+    result <- render p
+    c <- getContext
+    let rWidth = result ^. imageL . to imageWidth
+        -- leftPaddingAmount = max 0 $ (c ^. availWidthL - rWidth) `div` 2
+        leftPaddingAmount = max 0 $ (c ^. availWidthL - rWidth)
+        paddedImage = translateX leftPaddingAmount $ result ^. imageL
+        off = Location (leftPaddingAmount, 0)
+    if leftPaddingAmount == 0
+      then
+        return result
+      else
+        return $
+          addResultOffset off $
+            result & imageL .~ paddedImage
+
+-- | Counterpart to vCenterLayer that aligns to the bottom.
+--
+-- Mostly copied from `vCenterLayer`
+vAlignBottomLayer :: Widget n -> Widget n
+vAlignBottomLayer p =
+  Widget (hSize p) Greedy $ do
+    result <- render p
+    c <- getContext
+    let rHeight = result ^. imageL . to imageHeight
+        -- topPaddingAmount = max 0 $ (c ^. availHeightL - rHeight) `div` 2
+        -- TODO WIP Review, then use it for the help box
+        topPaddingAmount = max 0 $ (c ^. availHeightL - rHeight)
+        paddedImage = translateY topPaddingAmount $ result ^. imageL
+        off = Location (0, topPaddingAmount)
+    if topPaddingAmount == 0
+      then
+        return result
+      else
+        return $
+          addResultOffset off $
+            result & imageL .~ paddedImage
+
+-- | Align bottom-right
+alignBottomRightLayer :: Widget n -> Widget n
+alignBottomRightLayer = vAlignBottomLayer . hAlignRightLayer
