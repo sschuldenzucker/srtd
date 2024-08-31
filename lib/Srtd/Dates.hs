24df225efa67241f0b074a5c3801e87ccf0d9dee
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2024-08-31 14:01:56 +0200

    HumanDate parsing

--- lib/Srtd/Dates.hs
+++ lib/Srtd/Dates.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Srtd.Dates where
 
-import Control.Monad (void)
+import Control.Monad (void, when)
 import Control.Monad.Combinators.NonEmpty qualified as NE
-import Data.Maybe (isNothing)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Maybe (fromMaybe, isNothing)
+import Data.Ratio ((%))
 import Data.Text (Text)
-import Data.Time.Calendar
-import Data.Time.Calendar.Month
-import Data.Time.Clock
+import Data.Time
 import Data.Void
+import Srtd.Todo
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
@@ -15,7 +19,7 @@ import Text.Megaparsec.Char
 --
 -- No Ord instance is provided because *different* behaviors (beginning of day vs end of day) are
 -- expected in different situations.
-data DateOrTime = DateOrTime Day (Maybe UTCTime) deriving (Eq, Show)
+data DateOrTime = OnlyDate Day | DateAndTime UTCTime deriving (Eq, Show)
 
 -----------------------------
 -- Parsing
@@ -24,54 +28,214 @@ data DateOrTime = DateOrTime Day (Maybe UTCTime) deriving (Eq, Show)
 -- The following contain methods to parse a free-form description.
 -- This is intentionally incomplete, just what I use.
 
--- TODO I think this sucks a bit. Our parser should prob do this.
-
--- (Nothing, Nothing) is invalid and the parser will never return this.
+-- | A date and/or time reference expressed by a human.
+--
+-- This may look structurally a bit funky. For example, 'in 1 day' is structurally in a different
+-- place than 'in 1 hour'. This is because humans likely mean different things by these: 'in 1 hour' is
+-- a precise time difference, but 'in 1 day' could refer to the beginning or end of tomorrow depending
+-- on the situation.
 data HumanDateOrTime
-  = HumanDateOrTime (Maybe HumanDay) (Maybe HumanTime)
+  = -- | Date (relative or absolute), optionally time.
+    HDateMaybeTime HumanDate (Maybe TimeOfDay)
+  | -- | Time absolute with in an implicit day. Like "17:00"
+    HTimeOnly TimeOfDay
+  | -- | Relative time difference, like "in 1h"
+    HDiffTime NominalDiffTime
   deriving (Eq, Show)
 
-data HDRelativeNext
-  = NextDayOfMonth Int
-  | NextWeekday DayOfWeek
-  | NextMonth Month
+data HumanDate
+  = -- | Specific date, independent of a reference date.
+    HDAbs Day
+  | -- | Next relative anchor, e.g., 'in 2 mon' = 2nd monday from now.
+    -- NB this allows some weird but formally ok constructions like 'in 2 11-23' = 2 years from now on 11-23.
+    -- Some of these may still fail to apply later, e.g., if we want the 31st but it's April right now.
+    HDToNext Int DateAnchor
+  | -- | Relative difference, e.g., 'in 2 days' or 'tom'
+    HDDiff CalendarDiffDays
   deriving (Eq, Show)
 
-data HumanDay
-  = HDAbsolute {hdYear :: Maybe Int, hdMonth :: Maybe Int, hdDay :: Maybe Int}
-  | HDRelativeNext HDRelativeNext
-  | HDRelativeOffset DiffTime
+data DateAnchor
+  = AnchorDayOfWeek DayOfWeek
+  | AnchorDayOfMonth Int
+  | AnchorMonthDay Int Int
   deriving (Eq, Show)
-
-data HumanTime = HumanTime deriving (Eq, Show) -- TODO
 
 type Parser = Parsec Void Text
 
 type MyParseErrorBundle = ParseErrorBundle Text Void
 
+-- | See `pHumanDateOrTime`
 parseHumanDateOrTime :: Text -> Maybe HumanDateOrTime
 parseHumanDateOrTime = parseMaybe pHumanDateOrTime
 
+-- | Parse a human-language date or time.
+--
+-- This is a pretty terse language rn, optimized to minimize the number of keystrokes rather than be intuitive.
+--
+-- By example (also see the tests):
+--
+-- - 2024-07-18
+-- - 24-7-18
+-- - 18th
+-- - 11-18
+-- - tod
+-- - tom
+-- - fri
+-- - mon 17:00
+-- - mon 17t  (short form for 17:00, *not* '17h'!)
+-- - in 2 days
+-- - 2d
+-- - in 3 hours
+-- - 3h
 pHumanDateOrTime :: Parser HumanDateOrTime
-pHumanDateOrTime = do
-  hday <- optional (try pHumanDay)
-  htime <- optional pHumanTime
-  if isNothing hday && isNothing htime
-    then fail "No input"
-    else return $ HumanDateOrTime hday htime
+pHumanDateOrTime = space >> (try pHDateMaybeTime <|> try pHTimeOnly <|> pHDiffTime) <* (space >> eof)
 
-pHumanDay :: Parser HumanDay
--- TODO dummy (but it works)
-pHumanDay = do
-  year <- pNonNegInt
-  void $ char '-'
-  month <- pNonNegInt
-  void $ char '-'
-  day <- pNonNegInt
-  return $ HDAbsolute (Just year) (Just month) (Just day)
+pHDateMaybeTime :: Parser HumanDateOrTime
+pHDateMaybeTime = do
+  hdate <- pHumanDate
+  mhtime <- optional (space1 >> pTimeOfDay)
+  return $ HDateMaybeTime hdate mhtime
 
-pHumanTime :: Parser HumanTime
-pHumanTime = fail "TODO Not Implemented"
+pHumanDate :: Parser HumanDate
+pHumanDate = do
+  try pHDAbs <|> try pHDToNext <|> pHDDiff
+
+-- | 13:00 or 13t as a short form.
+--
+-- Note that `13h` means "in 13 hours"!
+--
+-- SOMEDAY maybe we should change this, it's confusing. Prob would need to mandate 'in' for time offsets.
+pTimeOfDay :: Parser TimeOfDay
+pTimeOfDay = do
+  h <- pNonNegInt
+  mm <- try (string "t" >> return Nothing) <|> (string ":" >> Just <$> pNonNegInt)
+  when (h > 23) $ fail "Invalid hour"
+  when (mm > Just 59) $ fail "Invalid minute"
+  -- We don't support seconds b/c we don't need it rn.
+  return $ TimeOfDay h (fromMaybe 0 mm) 0
+
+-- | "2024-03-27" or "24-03-27" as a short form.
+pHDAbs :: Parser HumanDate
+pHDAbs = do
+  year <- fixYear <$> pNonNegInt
+  void $ char '-'
+  month <- pPosInt
+  void $ char '-'
+  day <- pPosInt
+  Just theDate <- return $ fromGregorianValid (toInteger year) month day
+  return $ HDAbs theDate
+  where
+    fixYear y = if y < 100 then 2000 + y else y
+
+pHDToNext :: Parser HumanDate
+pHDToNext =
+  try ((HDToNext 1) <$> pDateAnchor) <|> do
+    void $ optional (string "in" >> space)
+    n <- pPosInt
+    space
+    a <- pDateAnchor
+    return $ HDToNext n a
+
+pDateAnchor :: Parser DateAnchor
+pDateAnchor = try pDADayOfWeek <|> try pDADayOfMonth <|> pDAMonthDay
+
+dayOfWeekPairs :: [([Text], DayOfWeek)]
+dayOfWeekPairs =
+  [ (["mon"], Monday),
+    (["tue"], Tuesday),
+    (["wed"], Wednesday),
+    (["thu"], Thursday),
+    (["fri"], Friday),
+    (["sat"], Saturday),
+    (["sun"], Sunday)
+  ]
+
+pDADayOfWeek :: Parser DateAnchor
+pDADayOfWeek = AnchorDayOfWeek <$> keywords dayOfWeekPairs
+
+pDADayOfMonth :: Parser DateAnchor
+pDADayOfMonth = do
+  day <- pPosInt
+  -- This allows some wrong things like '2th' but is probably fine.
+  void $ choice ["st", "nd", "rd", "th"]
+  -- Basic sanity check. Not tight of course.
+  when (day > 31) $ fail "Invalid day of month"
+  return $ AnchorDayOfMonth day
+
+pDAMonthDay :: Parser DateAnchor
+pDAMonthDay = do
+  month <- pPosInt
+  void $ string "-"
+  day <- pPosInt
+  when (month > 12) $ fail "Invalid month"
+  when (day > 31) $ fail "Invalid day of month"
+  -- \^ We do *not* check the (day, month) combo here, b/c we can't anyways, b/c of leap years.
+  return $ AnchorMonthDay month day
+
+-- TODO Remember to implement today and tomorrow.
+pHDDiff :: Parser HumanDate
+pHDDiff = HDDiff <$> (try pToday <|> try pTomorrow <|> pProperOffset)
+  where
+    pToday = string "tod" >> return (CalendarDiffDays 0 0)
+    pTomorrow = string "tom" >> return (CalendarDiffDays 0 1)
+    pProperOffset = do
+      void $ optional (string "in" >> space)
+      n <- pNonNegInt
+      space
+      step <- pStep
+      let res = scaleCalendarDiffDays (toInteger n) step
+      return res
+    pStep :: Parser CalendarDiffDays
+    pStep =
+      -- The default step is 1 day
+      -- (disabled for now b/c it's a bit too ambiguous for my taste)
+      -- <|> (return $ CalendarDiffDays 0 1)
+      keywords
+        [ (["days", "day", "d"], CalendarDiffDays 0 1),
+          (["weeks", "week", "w"], CalendarDiffDays 0 7),
+          (["months", "month", "m"], CalendarDiffDays 1 0)
+        ]
+
+pHTimeOnly :: Parser HumanDateOrTime
+pHTimeOnly = HTimeOnly <$> pTimeOfDay
+
+-- Similar to `pHDDiff`
+pHDiffTime :: Parser HumanDateOrTime
+pHDiffTime = HDiffTime <$> properTimeOffset
+  where
+    properTimeOffset = do
+      void $ optional (string "in" >> space)
+      n <- pNonNegInt
+      space
+      step <- pStep
+      let res = scaleNominalDiffTime (toInteger n) step
+      return res
+    pStep :: Parser NominalDiffTime
+    pStep =
+      keywords
+        [ (["hours", "hour", "h"], fromInteger (60 * 60)),
+          -- We do *not* use 'm' as a short form for "minute" b/c it's already used by month. Prob fine, we don't use this really anyways.
+          (["minutes", "minute", "mins", "min"], fromInteger 60),
+          (["seconds", "second", "secs", "sec", "s"], fromInteger 1)
+        ]
+
+keywords :: [([Text], a)] -> Parser a
+keywords = choice . map (\(ss, v) -> try $ choice (map mkKeyword ss) >> return v)
+  where
+    -- NB notFollowedBy is important to resolve ambiguities.
+    -- For example, 'mon' (monday) and 'month' (unit of time in date) both occur and otherwise would be ambiguous.
+    mkKeyword s = try (string s) >> notFollowedBy alphaNumChar
+
+pPosInt :: Parser Int
+pPosInt = do
+  res <- pNonNegInt
+  when (res == 0) $ fail "Must be positive"
+  return res
 
 pNonNegInt :: Parser Int
 pNonNegInt = read <$> some digitChar
+
+scaleNominalDiffTime :: (Integral a) => a -> NominalDiffTime -> NominalDiffTime
+scaleNominalDiffTime n t = n' * t
+  where
+    n' = fromRational (toInteger n % 1)
--- test/Main.hs
+++ test/Main.hs
@@ -2,10 +2,12 @@
 
 module Main (main) where
 
+import Data.Text (Text)
+import Data.Time
 import Srtd.Dates
-import Srtd.Main qualified
 import Test.Tasty
 import Test.Tasty.HUnit
+import Text.Megaparsec as Parsec
 
 unit_additionTest :: TestTree
 unit_additionTest =
@@ -18,11 +20,56 @@ testTests = testGroup "Test tests" [unit_additionTest]
 dateTests :: TestTree
 dateTests = testGroup "Date" [parseHumanDateOrTimeTests]
 
+parseE p = mapLeft Parsec.errorBundlePretty . Parsec.parse p "none"
+
+parseDateE :: Text -> Either String HumanDateOrTime
+parseDateE = mapLeft Parsec.errorBundlePretty . Parsec.parse pHumanDateOrTime "none"
+
+-- Don't wanna depend on extra right now.
+mapLeft :: (a -> b) -> Either a c -> Either b c
+mapLeft f (Left x) = Left $ f x
+mapLeft f (Right y) = Right y
+
+-- | Better than raw `@?=` b/c multi-line errors are formatted better (e.g. for parsers).
+shouldBeRight (Left errstr) _ = assertFailure errstr
+shouldBeRight (Right x) expd = x @?= expd
+
 parseHumanDateOrTimeTests =
   testGroup
     "Parse HumanDateOrTime"
-    [ testCase "Parse ISO date" $
-        parseHumanDateOrTime "2024-03-07" @?= Just (HumanDateOrTime (Just $ HDAbsolute (Just 2024) (Just 3) (Just 7)) Nothing)
+    [ testCase "ISO date" $
+        parseDateE "2024-03-07" `shouldBeRight` (HDateMaybeTime (HDAbs $ fromGregorian 2024 3 7) Nothing),
+      testCase "ISO short form" $
+        parseDateE "24-03-07" `shouldBeRight` (HDateMaybeTime (HDAbs $ fromGregorian 2024 3 7) Nothing),
+      testCase "anchor to day of week" $
+        parseDateE "mon" `shouldBeRight` (HDateMaybeTime (HDToNext 1 $ AnchorDayOfWeek Monday) Nothing),
+      testCase "int-shifted anchor to day of week" $
+        parseDateE "in 2 mon" `shouldBeRight` (HDateMaybeTime (HDToNext 2 $ AnchorDayOfWeek Monday) Nothing),
+      testCase "day offset" $
+        parseDateE "in 2 days" `shouldBeRight` (HDateMaybeTime (HDDiff $ CalendarDiffDays 0 2) Nothing),
+      testCase "month offset" $
+        parseDateE "in 2 months" `shouldBeRight` (HDateMaybeTime (HDDiff $ CalendarDiffDays 2 0) Nothing),
+      testCase "day offset short form 1" $
+        parseDateE "in 2d" `shouldBeRight` (HDateMaybeTime (HDDiff $ CalendarDiffDays 0 2) Nothing),
+      testCase "today" $
+        parseDateE "tod" `shouldBeRight` (HDateMaybeTime (HDDiff $ CalendarDiffDays 0 0) Nothing),
+      testCase "tomorrow" $
+        parseDateE "tom" `shouldBeRight` (HDateMaybeTime (HDDiff $ CalendarDiffDays 0 1) Nothing),
+      testCase "day offset short form 2" $
+        parseDateE "2d" `shouldBeRight` (HDateMaybeTime (HDDiff $ CalendarDiffDays 0 2) Nothing),
+      testCase "day offset with time" $
+        parseDateE "in 2 days 17:00"
+          `shouldBeRight` (HDateMaybeTime (HDDiff $ CalendarDiffDays 0 2) (Just $ TimeOfDay 17 0 0)),
+      testCase "absolute time only" $
+        parseDateE "17:00" `shouldBeRight` (HTimeOnly (TimeOfDay 17 0 0)),
+      testCase "absolute time only, short form" $
+        parseDateE "17t" `shouldBeRight` (HTimeOnly (TimeOfDay 17 0 0)),
+      testCase "relative time only, hour" $
+        parseDateE "in 1 hour" `shouldBeRight` (HDiffTime $ fromInteger (60 * 60)),
+      testCase "relative time only, hour short" $
+        parseDateE "1h" `shouldBeRight` (HDiffTime $ fromInteger (60 * 60)),
+      testCase "relative time only, minute short" $
+        parseDateE "2min" `shouldBeRight` (HDiffTime $ fromInteger (60 * 2))
     ]
 
 tests :: TestTree
