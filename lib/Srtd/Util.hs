192e224f9550b024f1521fc72bb1ef9890ae45e7
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2024-08-31 17:57:29 +0200

    some utils

--- lib/Srtd/Util.hs
+++ lib/Srtd/Util.hs
@@ -1,6 +1,19 @@
--- | Collected simple utilities
+-- | Collected simple utilities. Most of them are prob in extra but w/e.
 module Srtd.Util where
+
+import Control.Monad ((<=<))
+
+maybeToEither :: a -> Maybe b -> Either a b
+maybeToEither err = maybe (Left err) Right
 
 -- | This is really defined for anything with a neutral element, in this case the neutral element being `pure ()`
 whenJust :: (Applicative m) => Maybe a -> (a -> m ()) -> m ()
 whenJust mx f = maybe (pure ()) f mx
+
+-- | Compose a function n times with itself (0 times being the identity)
+composeNTimes :: Int -> (a -> a) -> a -> a
+composeNTimes n f = foldr (.) id $ replicate n f
+
+-- | Monadic variant of `composeNTimes`.
+composeNTimesM :: (Monad m) => Int -> (a -> m a) -> a -> m a
+composeNTimesM n f = foldr (<=<) return $ replicate n f
