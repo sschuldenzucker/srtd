8de796533d8461abef4fbf7e1294c549af3a73bb
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2024-09-02 11:05:40 +0200

    rendering for deadline

--- lib/Srtd/Components/Attr.hs
+++ lib/Srtd/Components/Attr.hs
@@ -2,8 +2,10 @@
 module Srtd.Components.Attr where
 
 import Brick
+import Data.Time (ZonedTime)
 import Srtd.AppAttr
 import Srtd.Attr
+import Srtd.Dates
 
 -- NB it's slightly unfortunate that both Brick and us use the term 'Attr' to refer to different things, but w/e.
 
@@ -22,6 +24,27 @@ renderStatus sel a = case a of
   Open -> withAttr (root <> attrName "open") (str "o")
   where
     root = if sel then selectedItemRowAttr <> attrName "status" else attrName "status"
+
+renderDeadline :: ZonedTime -> Bool -> DateOrTime -> Widget n
+renderDeadline now sel dt = withAttr (rootattr <> subattr <> selattr) . str . prettyRelativeMed now $ dt
+  where
+    rootattr = attrName "date" <> attrName "deadline"
+    subattr
+      | dt `isBefore` now = attrName "overdue"
+      | dt `isTodayOf` now = attrName "today"
+      | dt `isTomorrowOf` now = attrName "tomorrow"
+      | otherwise = mempty
+    selattr = if sel then attrName "selected" else mempty
+
+-- SOMEDAY this is a bit wide and also too narrow for all dates. Can be fixed by implementing a
+-- display variant that loses information the further out, e.g., ignore time unless today, ignore
+-- year unless after this year, etc. We're semi doing this rn.
+renderMaybeDeadline :: ZonedTime -> Bool -> Maybe DateOrTime -> Widget n
+renderMaybeDeadline now sel = setWidth 10 . maybe almostEmptyWidget (renderDeadline now sel)
+  where
+    setWidth w = hLimit w . padRight Max
+    -- BUG WORKAROUND: Using emptyWidget or str "" instead doesn't grow right in setWidth. No idea why.
+    almostEmptyWidget = str " "
 
 renderMaybeStatus :: Bool -> Maybe Status -> Widget n
 renderMaybeStatus sel = maybe (str "-") (renderStatus sel)
